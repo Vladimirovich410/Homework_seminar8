9a12e168db8e87bd468a6c7d7b7c7b8682b257f2
Vladimir <vermakov410@gmail.com>
2023-06-27 00:25:07 +0300

    Задача 1 доделанная

--- Program.cs
+++ Program.cs
@@ -20,9 +20,58 @@ int rows = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите колличество столбцов массива");
 int colums = Convert.ToInt32(Console.ReadLine());
 
-int [,] array = new int [rows, colums];
+int [,] arr = new int [rows, colums];
 
-void FillArray(int [,] arr)
+void FillArray(int [,] array)
 {
-    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
 }
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            int maxPosition = j;
+            for (int k = j+1; k < array.GetLength(1); k++)
+            {
+                if(array[i, k] > array[i, maxPosition])
+                {
+                    maxPosition = k;
+                }
+            }
+            int temp = array[i, j];
+            array[i, j] = array[i, maxPosition];
+            array[i, maxPosition] = temp;
+        }
+    }
+}
+
+
+FillArray(arr);
+PrintArray(arr);
+SelectionSort(arr);
+Console.WriteLine("_____");
+PrintArray(arr);
+
+
